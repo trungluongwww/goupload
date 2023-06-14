a7a94ff99dcbe204c56a867f24810f5484a26038
trungluong <trungluong>
2023-06-14 17:24:59 +0700

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
             instances: 1, // number process of application
             max_memory_restart: "1G", // restart if it exceeds the amount of memory specified
             env:{
-                "HOST":"https://capstone-trungluong.com",
+                "HOST":"",
                 "PORT":":8080"
             }
         },
